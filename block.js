c5ed5ec734a057d43d5404819213ba0ff1e7f724
Faxiang Li <lifa0917@gmail.com>
2019-01-17 22:03:18 -0500

    Create block class with constructor

---
+++ block.js
@@ -0,0 +1,10 @@
+class Block {
+  constructor({ timestamp, lastHash, hash, data }) {
+    this.timestamp = timestamp;
+    this.lastHash = lastHash;
+    this.hash = hash;
+    this.data = data;
+  }
+}
+
+const block1 = new Block();
